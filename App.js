7920807cf98b2fa846f9b8d414d4096d6b91068a
sachinprajapati8604 <sachinprajapati599@gmail.com>
2024-08-12 10:07:52 +0530

    created html using React cdn in app.js

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-const heading = React.createElement('h1', { id: 'heading' }, 'I am from React element');
+// Simple creating element 
+const parent2 = React.createElement(
+    "div",
+    { class: "parent2" },
+    "This is from 2nd div"
+);
 
+//Nested
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("h1", { id: "heading" }, "I am heading ")
+);
 
+const parent3 = React.createElement(
+    "div",
+    { id: "parent3", class: "parent3", "data-ab": "mydata" },
+    React.createElement("div", { id: "row" }, React.createElement("div", { id: "col" }), React.createElement("p", { id: "text" }, 'I am paragram inside row>col>p')));
+
+console.log(parent) // it is an object
 // Rendering inside root with React DOM
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([parent, parent2, parent3]);
+// if have one node we can simply use like below
+// root.render(parent);
